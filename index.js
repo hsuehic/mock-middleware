77cd3d71d20fd498ab8f79eda0c5cc3a5c5c916f
xiaowei.xue <xiaowei.xue@shopee.com>
2019-07-22 18:27:26 +0800

    comment

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const chokidar = require('chokidar');
 const path = require('path');
 
-module.exports = function (mockDirectory) {
+module.exports = function(mockDirectory) {
   const watcher = chokidar.watch(mockDirectory);
   var re = new RegExp(mockDirectory);
-  watcher.on('ready', function () {
-    watcher.on('all', function () {
+  watcher.on('ready', function() {
+    watcher.on('all', function() {
       console.log('Clearing ' + mockDirectory + ' module cache from server');
-      Object.keys(require.cache).forEach(function (id) {
+      // remove all require caches under mock dir
+      Object.keys(require.cache).forEach(function(id) {
         if (re.test(id.replace(new RegExp('\\' + path.sep, 'g'), '/'))) {
           delete require.cache[id];
         }
@@ -15,7 +16,7 @@ module.exports = function (mockDirectory) {
     });
   });
 
-  return function (req, res, next) {
+  return function(req, res, next) {
     require(mockDirectory)(req, res, next);
   };
 };
