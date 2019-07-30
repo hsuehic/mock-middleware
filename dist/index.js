751a9695a1970e2af384a236a8ef8a95764dfc37
xiaowei.xue <xiaowei.xue@shopee.com>
2019-07-30 15:41:26 +0800

    issue: fixed typo

--- dist/index.js
+++ dist/index.js
@@ -18,7 +18,7 @@ function default_1(mockDirectory) {
     });
     return function (req, res, next) {
         try {
-            let filePath = req.path;
+            let filePath = req.path.substr(1);
             const v = require(filePath);
             if (typeof v === 'function') {
                 v.call(null, req, res, next);
